4e23a13e0bc9bdb1a3263d44796fca1e76ecc67b
Mitch Smith <mitchpsmith1998@gmail.com>
2023-07-29 13:40:40 +1000

    Use standard controller reference for unnamed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   get "/login", to: "sessions#new", as: "new_session"
   post "/login", to: "sessions#create", as: "create_session"
   get "/logout", to: "sessions#destroy", as: "destroy_session"
 
-  root to: "channels#index"
+  root "channels#index"
   get "/:channel_id", to: "channels#show", as: "show_channels"
 end
