4ad939918136f730357648487faa4217f0646f50
Mitch Smith <mitchpsmith1998@gmail.com>
2023-07-29 13:56:38 +1000

    Add chat to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,8 @@
 mario = User.create_with(name: "Mario", password: "123456").find_or_create_by!(email: "mario@example.com")
 luigi = User.create_with(name: "Luigi", password: "123456").find_or_create_by!(email: "luigi@example.com")
 
+chat = Channel.new(name: "Mario Bros Chat")
+
+mario.channels << chat
+luigi.channels << chat
+
