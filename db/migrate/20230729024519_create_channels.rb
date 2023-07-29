c54b9d21959fb2822eb580d81765f6c8ac841dca
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2023-07-29 13:44:45 +1000

    add name to channel

--- db/migrate/20230729024519_create_channels.rb
+++ db/migrate/20230729024519_create_channels.rb
@@ -1,6 +1,7 @@
 class CreateChannels < ActiveRecord::Migration[7.0]
   def change
     create_table :channels do |t|
+      t.string :name, null: false, limit: 50
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/structure.sql
+++ db/structure.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMA
 CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
 CREATE TABLE IF NOT EXISTS "users" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(50) NOT NULL, "email" varchar(50) NOT NULL, "password_digest" varchar NOT NULL, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
 CREATE TABLE sqlite_sequence(name,seq);
-CREATE TABLE IF NOT EXISTS "channels" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "user_id" integer NOT NULL, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL, CONSTRAINT "fk_rails_e3493648f1"
+CREATE TABLE IF NOT EXISTS "channels" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(50) NOT NULL, "user_id" integer NOT NULL, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL, CONSTRAINT "fk_rails_e3493648f1"
 FOREIGN KEY ("user_id")
   REFERENCES "users" ("id")
 );
