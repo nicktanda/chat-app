8ce4c76495a027b252f9814ed28679967fc4d615
Mitch Smith <mitchpsmith1998@gmail.com>
2023-07-29 13:40:53 +1000

    Add multiple users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 
-user = User.create_with(name: "Mario Brother", password: "123456").find_or_create_by!(email: "test@example.com")
+mario = User.create_with(name: "Mario", password: "123456").find_or_create_by!(email: "mario@example.com")
+luigi = User.create_with(name: "Luigi", password: "123456").find_or_create_by!(email: "luigi@example.com")
+
