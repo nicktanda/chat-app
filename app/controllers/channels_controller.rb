0addfeb49fae1e4e30b0ac4bf39ab780e3d83294
Mitch Smith <mitchpsmith1998@gmail.com>
2023-07-30 21:23:44 +1000

    Begin to render chat messages and flash alerts

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,11 +2,10 @@ class ChannelsController < ApplicationController
   before_action :load_channel, only: [:show]
 
   def index
-    render json: current_user.channels.map(&:as_json)
+    @channels = current_user.channels
   end
 
   def show
-    render json: @channel.messages.map(&:as_json)
   end
 
   private
@@ -14,7 +13,7 @@ class ChannelsController < ApplicationController
   def load_channel
     @channel = current_user.channels.find_by(id: params[:channel_id])
     unless @channel
-      redirect_to root_path, notice: "The channel doesn't exist (or you don't have permisson to see it)"
+      redirect_to root_path, notice: "The channel '#{params[:channel_id]}' doesn't exist (or you don't have permisson to see it)"
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,12 @@
 mario = User.create_with(name: "Mario", password: "123456").find_or_create_by!(email: "mario@example.com")
 luigi = User.create_with(name: "Luigi", password: "123456").find_or_create_by!(email: "luigi@example.com")
 
-chat = Channel.new(name: "Mario Bros Chat")
+if mario.channels.empty?
+  chat = Channel.new(name: "Mario Bros Chat")
 
-mario.channels << chat
-luigi.channels << chat
+  mario.channels << chat
+  luigi.channels << chat
 
-chat.messages.create(body: "Peach is missing! Here we go again 🙄....", author: luigi)
-
+  chat.messages.create(body: "Peach is missing! Here we go again 🙄....", author: luigi)
+  chat.messages.create(body: "Lets a goooo!", author: mario)
+end
